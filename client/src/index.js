9f6dac183995721cc41d9f5aa7cdd0044acf014b
Abdullah Bilal <79226329+abdullahbilal-code@users.noreply.github.com>
2025-07-22 14:33:08 +0000

    rmv routes from client app

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-const appointmentRoutes = require('./routes/appointmentRoutes');
-const availabilityRoutes = require('./routes/availabilityRoutes');
-const userRoutes = require('./routes/userRoutes');
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
